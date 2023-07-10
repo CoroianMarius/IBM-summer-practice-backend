e7baa4a0b10d9316b77cbc53bd0670e84ec57b7e
Vlad Guzun <mernapplications@gmail.com>
2023-07-10 09:18:08 -0700

    auth

--- src/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controllers/eventController');
-
-router.get('/', eventController.getEvents);
-router.post('/', eventController.createEvent);
-
-module.exports = router;
