6498baf1e516da1cd8d4ae48780bc720a650a4f4
Vlad Guzun <mernapplications@gmail.com>
2023-07-23 11:19:25 -0700

    changed group schema

--- src/models/Group.js
+++ src/models/Group.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const GroupSchema = new mongoose.Schema({
     name: {type: String, required: true,unique: [true, 'A group with this name exists'] },
     users: [String],
-    notifications: [String],
+    notifications: [{
+        title: String,
+        date: Date,
+    }],
 })
 
 const Group = mongoose.model('Group', GroupSchema)
