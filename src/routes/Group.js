e6e17666c6d34a174ad4b4e5c439d5c1f8b985c0
DaniConstantinescu <65397773+DaniConstantinescu@users.noreply.github.com>
2023-07-27 00:16:03 +0300

    Created function to get all users from a Group

--- src/routes/Group.js
+++ src/routes/Group.js
@@ -20,6 +20,30 @@ groupRouter.get('/', passport.authenticate('jwt', {session: false}), async(req,r
     }
 });
 
+
+//_________________________________________IA TOATE GRUPURILE USERULUI (LOGAT) ______________________________________________________________________________
+groupRouter.get('/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+      if (req.user.role !== 'admin' && req.user.role !== 'user') {
+        return res.status(403).json({ message: { msgBody: "Not an admin, can't get groups", msgError: true } });
+      }
+  
+      const groupName = req.params.name;
+  
+      const group = await Group.findOne({ name: groupName });
+  
+      if (!group) {
+        return res.status(404).json({ message: { msgBody: 'Group not found', msgError: true } });
+      }
+  
+      res.status(200).json({ users: group.users });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: { msgBody: 'Server error', msgError: true } });
+    }
+  });
+
+
 // make the post route for creating a group and adding the user to it
 //_________________________________________CREAZA UN GRUP NOU SI ADAUGA USERUL LOGAT IN EL______________________________________________________________________________
 
